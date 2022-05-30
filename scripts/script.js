2f7d984d2595288abb9495f1a40d1afb25d26c69
iKickedBatman <Dan_S3@outlook.com>
2022-05-30 15:05:02 +0100

    All tasks finished, just styling now

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let boxes = document.getElementById("boxes")
+let noOfBoxes = document.getElementById("noOfBoxes")
+let colors = ["Aqua", "LightCoral", "Blue", "Pink", "Salmon", "Turquoise", "SlateBlue", "LightGreen", "RoyalBlue"]
+noOfBoxes.addEventListener("input", generateBoxes)
+
+function generateBoxes(e){
+    boxes.innerHTML = "";
+    for (let i=0;i<e.target.value;i++){
+        let box=document.createElement("div")
+        boxes.appendChild(box)
+        box.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)]; 
+    }
+}
+
+
+
+
