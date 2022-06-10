1dd23aadd23baca7cef8298d710e147d7c3d6499
iKickedBatman <Dan_S3@outlook.com>
2022-06-10 13:14:32 +0100

    random colours

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 
 let boxes = document.getElementById("boxes")
 let noOfBoxes = document.getElementById("noOfBoxes")
-let colors = ["Aqua", "LightCoral", "Blue", "Pink", "Salmon", "Turquoise", "SlateBlue", "LightGreen", "RoyalBlue"]
+// let colors = ["Aqua", "LightCoral", "Blue", "Pink", "Salmon", "Turquoise", "SlateBlue", "LightGreen", "RoyalBlue"]
 noOfBoxes.addEventListener("input", generateBoxes)
 
 function generateBoxes(e){
@@ -10,10 +10,10 @@ function generateBoxes(e){
     for (let i=0;i<e.target.value;i++){
         let box=document.createElement("div")
         boxes.appendChild(box)
-        box.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)]; 
+        box.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})` 
     }
 }
 
-//Math.random will never generate '1'. 
+//Math.random will never generate '1'.
 
 
